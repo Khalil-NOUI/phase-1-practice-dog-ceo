8e1ca2757ed8d35f26d1004c44422aed7ced888f
khalil NOUI <noui.khalil.pr@gmail.com>
2024-12-03 16:51:15 +0100

    breed fetch

--- src/index.js
+++ src/index.js
@@ -1,33 +1,49 @@
-const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+const breedUrl = "https://dog.ceo/api/breeds/list/all";
+
+
+/// DomContent ///
 
 document.addEventListener("DOMContentLoaded", () => {
-    
-    imgFetcher(imgUrl)
+    fetcherAll(imgUrl, imgRender);
+    fetcherAll(breedUrl, breedRender);
+});
 
-})
-
-
-//fetch images
-async function imgFetcher(imgUrl) {
-
-    try {     
-        const res = await fetch(imgUrl)
-        if (!res.ok) throw new Error(`server not responding, Status : ${res.status}`)
-        const data = await res.json()
-        displayHelp(data)
+async function fetcherAll(ref, callBack) {
+    try {
+        const res = await fetch(ref);
+        if(!res.ok) throw new Error(`server not responding, status : ${res.status}`);
+        const data = await res.json();
+        callBack(data);
     }
-
-    catch (error) {
-        console.error("Error detected:", error.message);
-    }
+    catch(err) {
+        console.log("error !", err.message);
+    };
 }
 
-function displayHelp(data) {
-    const imgDisplay = document.getElementById("dog-image-container")
+
+
+// Helper Function//
+
+function imgRender(data) {
+    const imgDisplayer = document.getElementById("dog-image-container");
+    const fragment = document.createDocumentFragment()
     data.message.forEach((url) => {
-        const img = document.createElement('img')
-        img.setAttribute("src", `${url}`)
-        img.setAttribute("alt", `dog picture`)
-        imgDisplay.appendChild(img)
+        const img = document.createElement('img');
+        img.src = url;
+        img.alt = `dog picture`;
+        fragment.appendChild(img);
+    });
+    imgDisplayer.appendChild(fragment)
+};
+
+function breedRender(data) {
+    const breedDisplayer = document.getElementById("dog-breeds");
+    const fragment = document.createDocumentFragment()
+    Object.keys(data.message).forEach((ele) => {
+        const breedItem = document.createElement("li");
+        breedItem.textContent = `${ele}`
+        breedDisplayer.appendChild(breedItem)
     })
-}
+    breedDisplayer.appendChild(fragment)
+};
